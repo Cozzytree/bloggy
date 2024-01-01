0c75a7fa7e8e0d7bcc2fc96aa805a1a441edaa45
cozzytree <hootowldhrubo@gmail.com>
2024-01-02 00:02:13 +0530

    env

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://vozbqbvaultodqeuimqv.supabase.co";
-export const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvemJxYnZhdWx0b2RxZXVpbXF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE2MTY2OTcsImV4cCI6MjAxNzE5MjY5N30.PKc51wQgjn5Do7NTl7Rps2Po3uSosHHlAKDzuf2H5Ag";
+export const supabaseUrl = import.meta.env.VITE_SUPABASEURL;
+export const supabaseKey = import.meta.env.VITE_SUPABASEKEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
